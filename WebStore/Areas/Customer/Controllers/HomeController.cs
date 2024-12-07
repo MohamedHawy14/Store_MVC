8e1799056e0a5d85e86fe6f91cd5d188108e539a
MohamedHawy <mohamedhawy2003@gmail.com>
2024-12-08 01:22:27 +0200

    Details $ Home Page

--- WebStore/Areas/Customer/Controllers/HomeController.cs
+++ WebStore/Areas/Customer/Controllers/HomeController.cs
@@ -1,20 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Models.Models;
+using DataAcess.Repository.IReository;
 namespace WebStore.Areas.Customer.Controllers
 {
+   
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+
+            IEnumerable<Product> productList = _unitOfWork.product.GetAll(IncludeProperties: "Category");
+            return View(productList);
+        }
+        public IActionResult Details(int Id)
+        {
+
+            var Product = _unitOfWork.product.Get(u => u.Id == Id, IncludeProperties: "Category");
+            if (Product == null)
+            {
+                return NotFound(); 
+            }
+
+            return View(Product);
         }
 
         public IActionResult Privacy()
